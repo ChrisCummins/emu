2afdd8ee21702b68dba96f2d43f2912975658b1d
Chris Cummins <christopher.e.cummins@intel.com>
2013-03-06 21:59:13 +0000

    scripts/mktar: Fix usage information

--- scripts/mktar.sh
+++ scripts/mktar.sh
@@ -2,10 +2,42 @@
 
 set -e
 
+print_help ()
+{
+    cat <<EOF
+Usage: ./scripts/mktar.sh [--version] [--help]
+
+Automatic packaging tool, generates a versioned release tarball and
+checksum. For example, if the current version is 0.3.0, then the
+script will generate two files:
+
+  ./releases/emu-0.3.0.tar.gz      # The release tarball.
+  ./releases/emu-0.3.0.tar.gz.md5  # Release tarball checksum.
+EOF
+}
+
+print_version ()
+{
+    echo "version $version"
+}
+
 version=$(cat lib/libemu | grep EMU_VERSION \
     | sed -r 's/.+\sEMU_VERSION="([0-9]+\.[0-9]+\.[0-9]+)"\s*$/\1/')
 
 test -n "$version" || { echo "Could not get version number!" >&2; exit 2; }
+
+for arg in $@; do
+    case "$arg" in
+        "--help")
+            print_help
+            exit 0
+            ;;
+        "--version")
+            print_version
+            exit 0
+            ;;
+    esac
+done
 
 # Create a release tarball.
 cd ..
