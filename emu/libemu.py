4e6a1aa04dc817737fa191433954c0854741a624
Chris Cummins <chrisc.101@gmail.com>
2014-07-08 16:48:54 +0100

    Bump release version for '0.1.6'

--- emu/libemu.py
+++ emu/libemu.py
@@ -830,7 +830,7 @@ class Util:
     #
     # Version information:
     #
-    version = { "major": 0, "minor": 1, "micro": 5 }
+    version = { "major": 0, "minor": 1, "micro": 6 }
     version_string = "{0}.{1}.{2}".format(version["major"],
                                           version["minor"],
                                           version["micro"])
