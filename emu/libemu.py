efae186659751811833377ab21e6b63f6bd8bea7
Chris Cummins <chrisc.101@gmail.com>
2014-08-30 04:15:14 +0100

    Bump release version for '0.1.34'

--- emu/libemu.py
+++ emu/libemu.py
@@ -1088,7 +1088,7 @@ class Emu:
     #
     # Version and copyright information:
     #
-    version = Version(0, 1, 33)
+    version = Version(0, 1, 34)
     copyright = { "start": 2012, "end": 2014, "authors": ["Chris Cummins"]}
 
     #
