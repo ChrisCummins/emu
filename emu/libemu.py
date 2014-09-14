5cfc2915751154edb24b9813c1d0c66986aa711c
Chris Cummins <chrisc.101@gmail.com>
2014-09-14 15:41:24 +0100

    libemu: Set version dirty flag

--- emu/libemu.py
+++ emu/libemu.py
@@ -1116,7 +1116,7 @@ class Emu:
     #
     # Version and copyright information:
     #
-    version = Version(0, 1, 49, dirty=False)
+    version = Version(0, 1, 49, dirty=True)
     copyright = { "start": 2012, "end": 2014, "authors": ["Chris Cummins"]}
 
     #
